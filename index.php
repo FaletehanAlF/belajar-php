541de59c7699363adc6192098b51e398112c4ab5
FaletehanAlF <faletehanalfarabi09>
2026-08-25 21:43:07 +0700

    update code

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "Hello, PHP Native!";
+$name = "Faletehan";
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1>Hello <?= $name ?></h1>
+
+    <form method="POST">
+    <input type="text" name="name">
+    <button type="submit">Kirim</button>
+</form>
+
+</body>
+</html>
